bccfd2ad8af253f0436de7893b0b3a9c446434f3
Flávio Conte <fhconte@gmail.com>
2018-04-11 23:09:07 -0300

    YT Search Response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
 
 const API_KEY =  'AIzaSyD_ZulSJwru98hAHU7CfJ6LblTYIKZcNIc';  
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
