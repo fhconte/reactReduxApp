bd52127f1620be8762fc06a8d0adf0487014349f
Flávio Conte <fhconte@gmail.com>
2018-04-14 16:51:05 -0300

    Throttling Search Term Input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,7 +17,11 @@ class App extends Component {
 
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+       this.videoSearch('Surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
             console.log(videos)
             this.setState({ 
                 videos: videos,
@@ -26,9 +31,11 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term =>this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
